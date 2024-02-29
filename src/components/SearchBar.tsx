86ef3555e3e7a4377c69287c284eac8c54f972ac
Lucas Jiang <lucas@interactivelabs.com>
2024-03-01 01:10:46 +0800

    add search icon for search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,59 +1,69 @@
-'use client'
+"use client";
 
-import { Autocomplete, TextField, Box, SelectChangeEvent } from "@mui/material";
+import {
+  Autocomplete,
+  TextField,
+  Box,
+  InputAdornment,
+} from "@mui/material";
 import useTaiwanStockInfo, { StockInfoType } from "@/hooks/useTaiwanStockInfo";
 import { usePathname, useRouter } from "next/navigation";
 import useStock from "@/hooks/useStock";
 import { SyntheticEvent } from "react";
+import SearchIcon from "@mui/icons-material/Search";
 
+export default function SearchBar() {
+  const { stocks } = useTaiwanStockInfo();
+  const router = useRouter();
 
-export default function SearchBar(){
-  const {stocks} = useTaiwanStockInfo()
-  const router = useRouter()
+  const pathname = usePathname();
+  const { selectStock } = useStock();
 
-  const pathname = usePathname()
-  const { selectStock } = useStock()
-
-  const handleChange = (event: SyntheticEvent<Element, Event>, value: StockInfoType | null) => {
-    if(value) {
-      selectStock(value)
-      router.push(`/analysis/${value?.stock_id}/${pathname.split('/').pop()}`)
+  const handleChange = (
+    event: SyntheticEvent<Element, Event>,
+    value: StockInfoType | null
+  ) => {
+    if (value) {
+      selectStock(value);
+      router.push(`/analysis/${value?.stock_id}/${pathname.split("/").pop()}`);
     }
-  }
-
-
+  };
 
   return (
     <header className="p-2 bg-white">
-      <Autocomplete 
-        size='small' 
-        sx={{margin: 'auto', width: 300}}
-        options={stocks? stocks.data : []} 
+      <Autocomplete
+        size="small"
+        sx={{ margin: "auto", width: 300 }}
+        options={stocks ? stocks.data : []}
         autoHighlight
-        getOptionLabel={(option:StockInfoType) => option.stock_id}
+        disableClearable
+        forcePopupIcon={false}
+        getOptionLabel={(option: StockInfoType) => option.stock_id}
         renderOption={(props, option: StockInfoType) => (
           <Box component="li" {...props}>
             {option.stock_id} {option.stock_name} {option.industry_category}
           </Box>
         )}
         onChange={handleChange}
-        renderInput={params => {
+        renderInput={(params) => {
+          console.log(params);
           return (
-            <TextField 
+            <TextField
               {...params}
               size="small"
               placeholder="輸入台／美股代號，查看公司價值"
-              // InputProps={{
-              //   endAdornment: (
-              //     <InputAdornment position="end">
-              //       <SearchIcon />
-              //     </InputAdornment>
-              //   ),
-              // }}
+              InputProps={{
+                ...params.InputProps,
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              }}
             />
-          )
-        }} 
+          );
+        }}
       />
     </header>
-  )
+  );
 }
